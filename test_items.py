55281b14071e10fee0f2963c2c76e00435fe4e66
Babich M <astilba22@gmail.com>
2020-07-21 22:11:01 +0300

    Add test

---
+++ test_items.py
@@ -0,0 +1,10 @@
+import time
+
+link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+
+
+def test_button_exists(browser):
+    browser.implicitly_wait(10)
+    browser.get(link)
+    time.sleep(15)
+    assert len(browser.find_elements_by_class_name('btn-add-to-basket1')) != 0, 'The button does not exist.'
